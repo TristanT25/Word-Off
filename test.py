a7110dc5102c5622e3246c2854d871c3c5007dfc
TristanT25 <90428018+TristanT25@users.noreply.github.com>
2022-09-24 16:08:21 -0500

    Update test.py

---
+++ test.py
@@ -0,0 +1,10 @@
+"""Test file to check if Dictionary work"""
+dictionary = []
+
+word = input("Enter a word to find in the dictionary: ")
+
+if word in dictionary:
+    print("This word is in the dictionary")
+
+else:
+    print("This word is not in the dictionary")
